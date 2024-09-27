d155b08ba83c217b3b7402b7577c99e421b8c046
Bench <boltlander@gmail.com>
2024-09-27 14:51:00 -0600

    added functional programming assignment

---
+++ functional-programming/hello-world-scala/src/main/scala/HelloWorldServer.scala
@@ -0,0 +1,49 @@
+// src/main/scala/HelloWorldServer.scala
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives._
+import spray.json.DefaultJsonProtocol._
+import scala.io.StdIn
+
+object HelloWorldServer {
+
+  def main(args: Array[String]): Unit = {
+    // Create an ActorSystem
+    implicit val system = ActorSystem(Behaviors.empty, "helloWorldSystem")
+    implicit val executionContext = system.executionContext
+
+    // Define the route
+    val route =
+      path("greet" / Segment) { person =>
+        get {
+          complete(s"Hello, $person!")
+        }
+      } ~
+      pathSingleSlash {
+        get {
+          complete(s"Hello!")
+        }
+      } ~
+      path("sortStrings") {
+        post {
+          entity(as[List[String]]) { stringList =>
+            // Sort the list using functional programming style (using `sorted` method)
+            val sortedList = stringList.sorted
+            complete(sortedList)
+          }
+        }
+      }
+
+    // Start the server
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+    println("Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // Keep the server running until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // Unbind from the port
+      .onComplete(_ => system.terminate()) // Terminate the system when done
+  }
+}
